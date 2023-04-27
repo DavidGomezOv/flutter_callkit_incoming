7cfe4897ee21d26ce1c2df406875d2f43de17df6
DavidGomezPibox <dgomez@pibox.app>
2023-04-27 17:57:33 -0600

    Changed implementation

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitIncomingActivity.kt
@@ -66,13 +66,6 @@ class CallkitIncomingActivity : Activity() {
 
     }
 
-    class IncomingActivityClass {
-        companion object {
-            @SuppressLint("StaticFieldLeak")
-            var activity: Activity? = null
-        }
-    }
-
     inner class EndedCallkitIncomingBroadcastReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (!isFinishing) {
@@ -114,7 +107,6 @@ class CallkitIncomingActivity : Activity() {
             window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
             window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD)
         }
-        IncomingActivityClass.activity = this@CallkitIncomingActivity
         transparentStatusAndNavigation()
         setContentView(R.layout.activity_callkit_incoming)
         initView()
